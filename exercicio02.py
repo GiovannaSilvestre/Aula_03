b9a4937884da2bcce9487afd01306084cdeef695
GiovannaSilvestre <giovanna.nsilvestre@hotmail.com>
2025-03-31 15:31:14 -0300

    Quatro operações de dois numeoros dados

---
+++ exercicio02.py
@@ -0,0 +1,10 @@
+valor1=float (input ("Qual é o primeiro número?"))
+valor2=float (input ("Qual é o segunda número?"))
+soma=(valor1+valor2)
+subtração=(valor1-valor2)
+multiplicação=(valor1*valor2)
+divisão=(valor1/valor2)
+print= (f"A soma dos valores é {soma}"
+f"A subtração dos valores é {subtração}"
+f"A multiplicação dos valores é {multiplicação}"
+f"A divisão dos valores é {divisão}")
--- exercício01.py
+++ exercício01.py
@@ -7,4 +7,4 @@ print(f"O seu nome é {nome},Sua idade é {idade}, Seu salário é {salario}")
 #aumento percentual  do salário
 porcentagem=float(input("Qual foi o aumento percentual do seu salário?"))
 salariofinal=float(((porcentagem+100)*salario)/100)
-print(f"O seu novo salário é {salariofinal}
+print(f"O seu novo salário é {salariofinal})
