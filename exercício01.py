a9dc9b7eaaed8e151102a32cd24fe7e6a118a94b
GiovannaSilvestre <giovanna.nsilvestre@hotmail.com>
2025-03-31 14:54:42 -0300

    Aula03ex02 - Adiciona a porcentagem e mostra o novo valor

--- exercício01.py
+++ exercício01.py
@@ -4,4 +4,7 @@ idade=int (input("Qual é a sua idade?"))
 salario=float(input("Qual é o seu salário?"))
 print(f"O seu nome é {nome},Sua idade é {idade}, Seu salário é {salario}")
 
-
+#aumento percentual  do salário
+porcentagem=float(input("Qual foi o aumento percentual do seu salário?"))
+salariofinal=float(((porcentagem+100)*salario)/100)
+print(f"O seu novo salário é {salariofinal}
